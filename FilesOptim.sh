8a7d0401ec28a8369351637e94b01797e3f6e7eb
SKOH <corentin.michel@mailo.com>
2022-01-14 17:26:10 +0100

    Big update !!

--- FilesOptim.sh
+++ FilesOptim.sh
@@ -1,223 +1,341 @@
- #!/bin/bash
- #########################################################################
- ## A shell script made to optimize JPG, PNG, videos bnd PDF files’ sizes.
- ## Copyright (C) Corentin Michel - All Rights Reserved
- ## Contact: corentin.michel@mailo.com [https://github.com/SKOHscripts]
- ## With help from Maximilian Fries’s code (2016) @MokaMokiMoke[https://github.com/MokaMokiMoke]
- #########################################################################
-
- rouge='\e[1;31m'
- vert='\e[1;33m'
- bleu='\e[1;34m'
- violet='\e[1;35m'
- vert='\e[1;32m'
- neutre='\e[0;m'
+#!/bin/bash
+#########################################################################
+## A shell script made to optimize JPG, PNG, videos bnd PDF files’ sizes.
+## Copyright (C) Corentin Michel - All Rights Reserved
+## Contact: corentin.michel@mailo.com [https://github.com/SKOHscripts]
+## With help from Maximilian Fries’s code (2016) @MokaMokiMoke[https://github.com/MokaMokiMoke]
+#########################################################################
+clear
+rouge='\e[1;31m'
+vert='\e[1;33m'
+bleu='\e[1;34m'
+violet='\e[1;35m'
+vert='\e[1;32m'
+neutre='\e[0;m'
 
 
 if [ "$UID" -eq "0" ]
 then
-  zenity --warning --height 80 --width 400 --title "EREUR" --text "Merci de lancez le script sans sudo : \n<b>./FilesOptim.sh</b>."
-  exit
+ zenity --warning --height 80 --width 400 --title "EREUR" --text "Merci de lancez le script sans sudo : \n<b>./FilesOptim.sh</b>."
+ exit
 fi
 
 which zenity > /dev/null
 if [ $? = 1 ]
 then
-  sudo apt install -y zenity
+ sudo apt install -y zenity
 fi
 
 zenity --question --width=500 --height=100 --title  "FilesOptim" --text "This script will recursively optimise the size of .JPG and .PNG files without loss of quality, some video files before converting them to .MP4 and finally .PDF files without loss of colour quality and fixing the images at 300DPI.\n\nAre you OK with that ?"
 if [ $? == 0 ]
 then
-  zenity --info --width=300 --height=100 --text "Please select the folder from where you want to start optimization."
-  inputStr=$(zenity --file-selection --directory "${HOME}")
-  cd $inputStr || zenity --error --width=300 --height=100 --text "The folder name has to have no spacces." && exit
-   ####################################################################################
-   # PICTURES OPTIMISATION
-   ####################################################################################
-   echo ""
-   echo -e -n "$vert [1/3]$rouge PICTURES OPTIMISATION "
-   for i in `seq 30 $COLUMNS`;
-       do echo -n "."
-   done
+ zenity --info --width=300 --height=100 --text "Please select the folder from where you want to start optimization."
+ inputStr=$(zenity --file-selection --directory "${HOME}")
+ cd $inputStr || zenity --error --width=300 --height=100 --text "The folder name has to have no spacces." || exit
+
+ ####################################################################################
+ # PICTURES OPTIMISATION
+ ####################################################################################
+ echo ""
+ echo -e -n "$vert [1/3]$rouge PICTURES OPTIMISATION "
+ for i in `seq 30 $COLUMNS`;
+   do echo -n "."
+ done
+ echo -e " $neutre"
+ sleep 3
+
+ which jpegoptim > /dev/null
+ if [ $? = 1 ]
+ then
+  sudo apt install -y jpegoptim
+ fi
+
+ {
+ count=0
+ success=0
+ successlog=./success.tmp
+ find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) >> jpg_files.txt
+ total=$(wc -l < "jpg_files.txt")
+ log=./log
+ gainlog=./gain.tmp
+ echo "0" | tee $successlog $gainlog > /dev/null
+ }
+
+ IFS=$'\n'       # make newlines the only separator
+ set -f          # disable globbing
+
+ if [ ! -s "./jpg_files.txt" ]
+ then
+   echo -e " $violet"
+   echo "No JPG File in Directory"
    echo -e " $neutre"
-   sleep 3
 
-   which jpegoptim > /dev/null
-   if [ $? = 1 ]
-   then
-   	sudo apt install -y jpegoptim
-   fi
-
-   find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) >> jpg_files.txt
-   IFS=$'\n'       # make newlines the only separator
-   set -f          # disable globbing
+ else
    for i in $(cat < "./jpg_files.txt"); do
-     echo "$i"
+     ((count++))
+     echo "Processing File #$count of $total Files"
+     echo "Current File: $i "
+     sizeold=$(wc -c "$i" | cut -d' ' -f1)
      jpegoptim -p -t "$i"
+     sizenew=$(wc -c "$i" | cut -d' ' -f1)
+     difference=$((sizenew-sizeold))
+     # Check if new filesize is smaller
+     if [ $difference -lt 0 ]
+     then
+       printf "Compression was successfull. New File is %'.f Bytes smaller\n" \
+       $((-difference)) | tee -a $log
+       ((success++))
+       echo $success > $successlog
+       ((gain-=difference))
+       echo $gain > $gainlog
+     else
+       echo "Compression was not necessary" | tee -a $log
+     fi
    done
-   if [ ! -s "./jpg_files.txt" ]
-   then
-     echo -e " $violet"
-     echo "No JPG File in Directory"
-     echo -e " $neutre"
-   fi
-  rm ./jpg_files.txt
+   # Print Statistics
+   printf "Successfully compressed %'.f of %'.f files\n" $(cat $successlog) $total | tee -a $log
+   printf "Safed a total of %'.f Bytes\n" $(cat $gainlog) | tee -a $log
+ fi
+ rm ./jpg_files.txt
 
-  which optipng > /dev/null
-  if [ $? = 1 ]
-  then
-   sudo apt install -y optipng
-  fi
+ sleep 2
+ ####################################################################################
 
-   find . -type f \( -iname "*.png" \) >> png_files.txt
-   IFS=$'\n'       # make newlines the only separator
-   set -f          # disable globbing
+ which optipng > /dev/null
+ if [ $? = 1 ]
+ then
+  sudo apt install -y optipng
+ fi
+
+ {
+ count=0
+ success=0
+ successlog=./success.tmp
+ find . -type f \( -iname "*.png" \) >> png_files.txt
+ total=$(wc -l < "png_files.txt")
+ # log=./log
+ # gainlog=./gain.tmp
+ echo "0" | tee $successlog > /dev/null
+ }
+
+ IFS=$'\n'       # make newlines the only separator
+ set -f          # disable globbing
+
+ if [ ! -s "./png_files.txt" ]
+ then
+   echo -e " $violet"
+   echo "No PNG File in Directory"
+   echo -e " $neutre"
+
+ else
    for i in $(cat < "./png_files.txt"); do
-     echo "$i"
+     ((count++))
+     echo "Processing File #$count of $total Files"
+     echo "Current File: $i "
+     sizeold=$(wc -c "$i" | cut -d' ' -f1)
      optipng -o5 -preserve -verbose "$i"
+     sizenew=$(wc -c "$i" | cut -d' ' -f1)
+     difference=$((sizenew-sizeold))
+     # Check if new filesize is smaller
+     if [ $difference -lt 0 ]
+     then
+       printf "Compression was successfull. New File is %'.f Bytes smaller\n" \
+       $((-difference)) | tee -a $log
+       ((success++))
+       echo $success > $successlog
+       ((gain-=difference))
+       echo $gain > $gainlog
+     else
+       echo "Compression was not necessary" | tee -a $log
+     fi
    done
-   if [ ! -s "./png_files.txt" ]
-   then
-     echo -e " $violet"
-     echo "No PNG File in Directory"
-     echo -e " $neutre"
-   fi
-  rm ./png_files.txt
+   # Print Statistics
+   printf "Successfully compressed %'.f of %'.f files\n" $(cat $successlog) $total | tee -a $log
+   printf "Safed a total of %'.f Bytes\n" $(cat $gainlog) | tee -a $log
 
-  #####################################################################################
-  # VIDEO OPTIMISATION
-  #####################################################################################
-  echo ""
-  echo -e -n "$vert [2/3]$rouge VIDEO OPTIMISATION "
-  for i in `seq 27 $COLUMNS`;
-      do echo -n "."
-  done
-  echo -e " $neutre"
-  sleep 3
-  find . -type f -iname "*.mp4" -o -iname '*.mkv' -o -iname '*.avi' -o -iname '*.m4v' -o -iname '*.wmv'>> paths_file.txt
+   rm $successlog $log
+ fi
+ rm ./png_files.txt
 
-  which ffmpeg > /dev/null
-  if [ $? = 1 ]
-  then
-   sudo apt install -y ffmpeg
-  fi
 
-  IFS=$'\n'       # make newlines the only separator
-  set -f          # disable globbing
-  for i in $(cat < "./paths_file.txt"); do
-    echo "$i"
-    ffmpeg -y -i "$i" -vcodec libx265 -crf 28 ${i%%c.*}.mp4 &&  mv ${i%%c.*}.mp4 "$i" || rm ${i%%c.*}.mp4
-  done
+ #####################################################################################
+ # VIDEO OPTIMISATION
+ #####################################################################################
+ echo ""
+ echo -e -n "$vert [2/3]$rouge VIDEO OPTIMISATION "
+ for i in `seq 27 $COLUMNS`;
+     do echo -n "."
+ done
+ echo -e " $neutre"
+ sleep 3
 
-  if [ ! -s "./paths_file.txt" ]
-  then
-    echo -e " $violet"
-    echo "No VIDEO File in Directory"
-    echo -e " $neutre"
-  fi
+ which ffmpeg > /dev/null
+ if [ $? = 1 ]
+ then
+  sudo apt install -y ffmpeg
+ fi
 
-  rm ./paths_file.txt
+ {
+ count=0
+ success=0
+ successlog=./success.tmp
+ gain=0
+ gainlog=./gain.tmp
+ find . -type f -iname "*.mp4" -o -iname '*.mkv' -o -iname '*.avi' -o -iname '*.m4v' -o -iname '*.wmv'>> paths_file.txt
+ total=$(wc -l < "paths_file.txt")
+ log=./log
+ echo "0" | tee $successlog > /dev/null
+ limit=75 #If video is compressed for less than 75%, the compression will be cancelled
+ }
 
-  #####################################################################################
-  # PDF OPTIMISATION
-  #####################################################################################
-  echo ""
-  echo -e -n "$vert [3/3]$rouge PDF OPTIMISATION "
-  for i in `seq 25 $COLUMNS`;
-      do echo -n "."
-  done
-  echo -e " $neutre"
-  sleep 3
-  ## Script to compress PDF Files using ghostscript incl. subdirs
-  ## Copyright (C) 2016 Maximilian Fries - All Rights Reserved
-  ## Contact: maxfries@t-online.de
-  ## Last revised 2022-01-05 by Corentin Michel (https://github.com/SKOHscripts)
+ IFS=$'\n'       # make newlines the only separator
+ set -f          # disable globbing
 
-  # Variables and preparation
+ if [ ! -s "paths_file.txt" ]
+ then
+   echo -e " $violet"
+   echo "No VIDEO File in Directory"
+   echo -e " $neutre"
+ else
+   for i in $(cat < "./paths_file.txt"); do
+     ((count++))
+     echo "Processing File #$count of $total Files"
+     echo "Current File: $i "
+     extension="${i##*.}"
+     new="$i.$extension"
+     ffmpeg -y -i "$i" -vcodec libx265 -crf 28 "$new" || rm "$new"
 
-  which gs > /dev/null
-  if [ $? = 1 ]
-  then
-   sudo apt install -y gs
-  fi
+     sizeold=$(wc -c "$i" | cut -d' ' -f1)
+     sizenew=$(wc -c "$i.$extension" | cut -d' ' -f1)
+     difference=$((sizenew-sizeold))
+     perc=$((-difference*100/sizeold))
 
-  {
-  count=0
-  success=0
-  successlog=./success.tmp
-  gain=0
-  gainlog=./gain.tmp
-  pdfs=$(find . -type f -name "*.pdf")
-  total=$(echo "$pdfs" | wc -l)
-  log=./log
-  verbose="-dQUIET"
-  mode="printer"
-  echo "0" | tee $successlog $gainlog > /dev/null
-  }
+     echo ""
 
-  # Are there any PDFs?
-  if [ "$total" -gt 0 ]; then
+     # Check if new filesize is smaller
+     if [ $perc -ge $limit ]
+     then
+       mv "$new" "$i"
+       printf "Compression was successfull. New File is %'.f Bytes smaller\n" \
+       $((-difference)) | tee -a $log
+       ((success++))
+       echo $success > $successlog
+       ((gain-=difference))
+       echo $gain > $gainlog
+     else
+       rm "$new"
+       echo "Compression was not necessary" | tee -a $log
+     fi
+   done
+   # Print Statistics
+   printf "Successfully compressed %'.f of %'.f files\n" $(cat $successlog) $total | tee -a $log
+   printf "Safed a total of %'.f Bytes\n" $(cat $gainlog) | tee -a $log
 
-    #Parameter Handling & Logging
-    {
-      echo "-- Debugging for Log START --"
-      echo "Number of Parameters: $#"
-      echo "Parameters are: $*"
-      echo "-- Debugging for Log END   --"
-    } >> $log
+   rm $successlog $log
+ fi
+ rm ./paths_file.txt
 
-    # Only compression-mode set
-    if [ $# -eq 1 ]; then
-      mode="$1"
-    fi
 
-    # Also Verbose Level Set
-    if [ $# -eq 2 ]; then
-      mode="$1"
-      verbose=""
-    fi
+ #####################################################################################
+ # PDF OPTIMISATION
+ #####################################################################################
+ echo ""
+ echo -e -n "$vert [3/3]$rouge PDF OPTIMISATION "
+ for i in `seq 25 $COLUMNS`;
+     do echo -n "."
+ done
+ echo -e " $neutre"
+ sleep 3
+ ## Script to compress PDF Files using ghostscript incl. subdirs
+ ## Copyright (C) 2016 Maximilian Fries - All Rights Reserved
+ ## Contact: maxfries@t-online.de
+ ## Last revised 2022-01-05 by Corentin Michel (https://github.com/SKOHscripts)
 
-    echo "$pdfs" | while read -r file
-    do
-      ((count++))
-      echo "Processing File #$count of $total Files" | tee -a $log
-      echo "Current File: $file "| tee -a $log
-      gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS="/$mode" -dNOPAUSE \
-      -dBATCH $verbose -sOutputFile="$file-new" "$file" | tee -a $log
+ # Variables and preparation
 
-      sizeold=$(wc -c "$file" | cut -d' ' -f1)
-      sizenew=$(wc -c "$file-new" | cut -d' ' -f1)
-      difference=$((sizenew-sizeold))
+ which gs > /dev/null
+ if [ $? = 1 ]
+ then
+  sudo apt install -y gs
+ fi
 
-      # Check if new filesize is smaller
-      if [ $difference -lt 0 ]
-      then
-        rm "$file"
-        mv "$file-new" "$file"
-        printf "Compression was successfull. New File is %'.f Bytes smaller\n" \
-        $((-difference)) | tee -a $log
-        ((success++))
-        echo $success > $successlog
-        ((gain-=difference))
-        echo $gain > $gainlog
-      else
-        rm "$file-new"
-        echo "Compression was not necessary" | tee -a $log
-      fi
+ {
+ count=0
+ success=0
+ successlog=./success.tmp
+ gain=0
+ gainlog=./gain.tmp
+ find . -type f -name "*.pdf" >> pdf_files.txt
+ total=$(wc -l < "pdf_files.txt")
+ log=./log
+ verbose="-dQUIET"
+ mode="printer"
+ echo "0" | tee $successlog > /dev/null
+ }
 
-    done
+ #Parameter Handling & Logging
+ {
+   echo "-- Debugging for Log START --"
+   echo "Number of Parameters: $#"
+   echo "Parameters are: $*"
+   echo "-- Debugging for Log END   --"
+ } >> $log
 
-    # Print Statistics
-    printf "Successfully compressed %'.f of %'.f files\n" $(cat $successlog) $total | tee -a $log
-    printf "Safed a total of %'.f Bytes\n" $(cat $gainlog) | tee -a $log
 
-    rm $successlog $gainlog
 
-  else
-    echo "No PDF File in Directory"
-  fi
+ # Only compression-mode set
+ if [ $# -eq 1 ]; then
+   mode="$1"
+ fi
+
+ # Also Verbose Level Set
+ if [ $# -eq 2 ]; then
+   mode="$1"
+   verbose=""
+ fi
+
+ IFS=$'\n'       # make newlines the only separator
+ set -f          # disable globbing
+
+ if [ ! -s "pdf_files.txt" ]
+ then
+   echo -e " $violet"
+   echo "No PDF File in Directory"
+   echo -e " $neutre"
+
+ else
+   for i in $(cat < "pdf_files.txt"); do
+     ((count++))
+     echo "Processing File #$count of $total Files" | tee -a $log
+     echo "Current File: $i "| tee -a $log
+     gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS="/$mode" -dNOPAUSE \ -dBATCH $verbose -sOutputFile="$i-new" "$i" | tee -a $log
+
+     sizeold=$(wc -c "$i" | cut -d' ' -f1)
+     sizenew=$(wc -c "$i-new" | cut -d' ' -f1)
+     difference=$((sizenew-sizeold))
+
+       # Check if new filesize is smaller
+       if [ $difference -lt 0 ]
+       then
+         rm "$i"
+         mv "$i-new" "$i"
+         printf "Compression was successfull. New File is %'.f Bytes smaller\n" \
+         $((-difference)) | tee -a $log
+         ((success++))
+         echo $success > $successlog
+         ((gain-=difference))
+         echo $gain > $gainlog
+       else
+         rm "$i-new"
+         echo "Compression was not necessary" | tee -a $log
+       fi
+       # Print Statistics
+       printf "Successfully compressed %'.f of %'.f files\n" $(cat $successlog) $total | tee -a $log
+       printf "Safed a total of %'.f Bytes\n" $(cat $gainlog) | tee -a $log
+   done
+ fi
+ rm $successlog $gainlog $log ./pdf_files.txt
 
 else
-    exit
+   exit
 fi
